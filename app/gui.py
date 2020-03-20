76f31f9dbd0b1f2364a223b6c683e40ccab29e8e
Francisco de Amorim III <fca3br@gmail.com>
2020-03-21 00:48:41 +0100

    First version

--- app/gui.py
+++ app/gui.py
@@ -3,6 +3,8 @@ from tkinter import font
 import RPi.GPIO as GPIO
 import configparser
 import os
+import time
+import threading
 
 #Read Settings
 parser = configparser.ConfigParser()
@@ -11,20 +13,38 @@ parser.read('config.ini')
 #Gloval Variables
 pin = 21
 appMode = 'IDLE'
+cwd = os.getcwd()
 
 armingTime = int(parser.get('Settings','ArmingSeconds'))
 activeUVTime = int(parser.get('Settings','ActiveUVSeconds'))
 
-cwd = os.getcwd()
-print(cwd)
 armingSound = parser.get('Settings','ArmingSoundFile')
 activeSound = parser.get('Settings','ActiveSoundFile')
+
+TIME = 0
 
 #Setting GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.OUT)
 GPIO.output(pin, GPIO.HIGH)
 
+def countDown():
+	global globalTimer, TIME
+		
+	if TIME>0:
+		mins, secs = divmod(TIME, 60)
+		timer = '{:02d}:{:02d}'.format(mins, secs)
+		timerLabel['text'] = timer
+		TIME = TIME-1
+		globalTimer = threading.Timer(1, countDown)
+		globalTimer.start()
+	else:
+		if appMode == 'ARMED':
+			activateUV()
+		elif appMode == 'ACTIVE':
+			disarmDevice()
+		
+	
 def displayMode(modeText):
 	global appMode
 	appMode = modeText
@@ -35,85 +55,120 @@ def displayMode(modeText):
 		timerLabel['text'] = '00:00'
 				
 	elif modeText == 'ARMED':
-		backColor = 'white'
-		textColor = 'black'
+		backColor = 'black'
+		textColor = 'red'
 		
 	elif modeText == 'ACTIVE':
-		backColor = 'black'
-		textColor = 'black'
+		backColor = 'light blue'
+		textColor = 'purple'
 	
 	win['background'] = backColor 
+	
 	armButton['background'] = backColor
+	armButton['highlightbackground'] = backColor
+	
 	resetButton['background'] = backColor
+	resetButton['highlightbackground'] = backColor
+	
 	exitButton['background'] = backColor
+	exitButton['highlightbackground'] = backColor
+	exitButton['fg'] = textColor
 	
-	armButton['highlightcolor'] = backColor
-	resetButton['highlightcolor'] = backColor
-	exitButton['highlightcolor'] = backColor
-	
-	statusLabel['text'] = modeText
-	statusLabel['fg'] = textColor
 	statusLabel['background'] = backColor
+	statusLabel['fg'] = textColor
+	statusLabel['text'] = modeText
 	
-	timerLabel['fg'] = textColor
 	timerLabel['background'] = backColor
+	timerLabel['fg'] = textColor
 	
-
-		
-def startTimer():
-	GPIO.cleanup()
-	win.quit()	
 
 def activateUV():
-	if GPIO.input(pin) :
-		GPIO.output(pin,GPIO.LOW)
-		armButton['text'] = 'LED ON'
-		
-	else:
-		GPIO.output(pin,GPIO.HIGH)
-		armButton['text'] = 'LED OFF'
+	global globalTimer, TIME
+	
+	#Set timer
+	TIME = activeUVTime
+	globalTimer = threading.Timer(0, countDown)
+	globalTimer.start()
+
+	#Set display mode
+	displayMode('ACTIVE')
+
+	os.system('pkill omxplayer')
+	command = 'omxplayer --loop -o local {0}/audios/{1} &'.format(cwd, activeSound)
+	os.system(command)
+
+	#Turn on Light
+	GPIO.output(pin,GPIO.LOW)
+	
 
 def armDevice():
-	GPIO.cleanup()
-	win.quit()	
+	global globalTimer, TIME
 
+	if appMode == 'IDLE':
+		#Set timer
+		TIME = armingTime
+		globalTimer = threading.Timer(0, countDown)
+		globalTimer.start()
+	
+	#Set display mode
+	displayMode('ARMED')
+	command = 'omxplayer --loop --vol -1000 -o local {0}/audios/{1} &'.format(cwd, armingSound)
+	os.system(command)
+	
+	
 def disarmDevice():
-	GPIO.cleanup()
-	win.quit()	
+	global globalTimer, TIME
+	
+	#Cancel Timer
+	globalTimer.cancel()
+
+	os.system('pkill omxplayer')
+	
+	#Turn off Light
+	GPIO.output(pin,GPIO.HIGH)
+	
+	#Set display mode
+	displayMode('IDLE')
+	
 
 def exitProgram():
+	disarmDevice()
+	
 	GPIO.cleanup()
 	win.quit()	
 
 # Create and configure Window
 win = Tk()
-win.geometry('480x320')
+#win.geometry('480x320')
+win.attributes("-fullscreen", True)
+
+globalTimer = threading.Timer(0.01, countDown)
 
 exitFont = font.Font(family = 'Helvetica', size = 16, weight = 'bold')
 statusFont = font.Font(family = 'Helvetica', size = 30, weight = 'bold')
 timerFont = font.Font(family = 'Helvetica', size = 50, weight = 'bold')
 
-armImg = PhotoImage(file = 'arming_button.png')
-armButton = Button(win, image=armImg, command=activateUV )
-armButton.pack()
-armButton.place(x=5,y=5)
-
-resetImg = PhotoImage(file = 'reset_button.png')
-resetButton = Button(win, image=resetImg )
-resetButton.pack()
-resetButton.place(x=480-110,y=320-110)
-
-exitButton  = Button(win, text = "Exit", font = exitFont, command = exitProgram, height =1 , width = 3) 
-exitButton.pack()
-exitButton.place(x=5,y=320-40)
-
-statusLabel  = Label(win, text = "Idle", font = statusFont, height =1 , width = 6) 
+statusLabel  = Label(win, text = "Idle", font = statusFont, height =1 , width = 7) 
 statusLabel.pack()
 statusLabel.place(x=480-180,y=5)
 
 timerLabel  = Label(win, text = "00:00", font = timerFont, height =1 , width = 5) 
 timerLabel.pack()
 timerLabel.place(x=480-200,y=60)
+
+armImg = PhotoImage(file = 'arming_button.png')
+armButton = Button(win, image=armImg, command=armDevice )
+armButton.pack()
+armButton.place(x=5,y=5)
+
+resetImg = PhotoImage(file = 'reset_button.png')
+resetButton = Button(win, image=resetImg, command=disarmDevice )
+resetButton.pack()
+resetButton.place(x=480-110,y=320-110)
+
+exitButton  = Button(win, text = "Exit", font = exitFont, command = exitProgram, height =1 , width = 3) 
+exitButton.pack()
+exitButton.place(x=5,y=320-40)
 
 displayMode('IDLE')
 mainloop()
